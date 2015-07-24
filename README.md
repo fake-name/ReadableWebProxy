3c23a7ec017b9224e3049d09761e73bd49dddb73
Fake-Name <something@fake-url.com>
2015-07-23 21:34:16 -0700

    Base bits stolen from wlnupdates project.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Readable-Web Proxy
+
+Reading long-form content on the internet is a shitty experience.   
+This is a web-proxy that tries to make it better.
---
+++ build-venv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# So ubuntu continues to spectacularly fuck up their python distro, in this
+# case the broke parts of pip (which is now baked into python 3.4 by default).
+# see https://bugs.launchpad.net/ubuntu/+source/python3.4/+bug/1290847
+
+# Anyways, build a venv that works.
+
+python3 -m venv --without-pip flask
+wget https://bootstrap.pypa.io/get-pip.py
+./flask/bin/python3 get-pip.py
+rm get-pip.py
+./flask/bin/pip install -r requirements.txt
