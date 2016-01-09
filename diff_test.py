878ad0ec6bd5068e1a2ca976a9fb7b79959e59da
Fake-Name <something@fake-url.com>
2016-01-08 20:58:42 -0800

    Working on diff stuff.

--- diff_test.py
+++ diff_test.py
@@ -3,26 +3,49 @@ if __name__ == '__main__':
 	import logSetup
 	logSetup.initLogging()
 
-import difflib
+import json
+import Misc.diff_match_patch as dmp
 import WebMirror.util.webFunctions as wf
 
 def go():
 	wg = wf.WebGetRobust()
 
-	p1 = wg.getpage("https://www.google.com/")
-	p2 = wg.getpage("https://www.google.com/")
+	new_content = wg.getpage("https://www.youtube.com/")
+	old_content = wg.getpage("https://www.youtube.com/")
 
 	print("Calculating diff")
-	d = difflib.unified_diff(p1.splitlines(1), p2.splitlines(1))
-	diff = "".join(list(d))
-	print(diff)
-	print(len(p1), type(p1))
-	print(len(p2), type(p2))
-	print(p1==p2)
 
-	print(d)
-	print(len(diff))
-	print(wg)
+	differ = dmp.diff_match_patch()
+
+	diff = differ.patch_make(new_content, old_content)
+	textdiff = differ.patch_toText(diff)
+	# print(textdiff)
+	print(len(new_content), len(old_content), len(diff), len(textdiff))
+	differ_2 = dmp.diff_match_patch()
+	d2     = differ_2.patch_fromText(textdiff)
+	old_content_reconstituted, results = differ_2.patch_apply(d2, new_content)
+
+	print(type(old_content_reconstituted))
+	print("Succeeded: ", old_content == old_content_reconstituted)
+
+
+	# err = differ_2.patch_make(new_content, old_content_reconstituted)
+	# print(err)
+	# print(d2)
+
+	# import code
+	# code.interact(local=locals())
+
+	# d = difflib.unified_diff(new_content.splitlines(1), old_content.splitlines(1))
+	# diff = "".join(list(d))
+	# print(diff)
+	# print(len(new_content), type(new_content))
+	# print(len(old_content), type(old_content))
+	# print(new_content==p2)
+
+	# print(d)
+	# print(len(diff))
+	# print(wg)
 
 
 if __name__ == '__main__':
