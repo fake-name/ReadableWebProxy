13a78186aa5959274fe422cb58ccc21a2b789678
Fake-Name <something@fake-url.com>
2016-05-29 20:41:29 -0700

    Fix execute perms, url in run.

--- nusync/run.py
+++ nusync/run.py
@@ -10,7 +10,8 @@ def go():
 
 	fetcher = NUSeriesUpdateFilter.NUSeriesUpdateFilter(db.session())
 	print(fetcher)
-	print(fetcher.handlePage("https://www.wlnupdates.com"))
+	# print(fetcher.handlePage("https://www.wlnupdates.com"))
+	print(fetcher.handlePage("https://www.novelupdates.com"))
 
 
 if __name__ == '__main__':
