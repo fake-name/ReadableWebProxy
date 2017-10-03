3819c26d6adee1d84d172ef413356157841cd396
Fake-Name <something@fake-url.com>
2017-10-02 23:19:33 -0700

    Miscellaneous sources.

--- WebMirror/PreProcessors/WixsitePreprocess.py
+++ WebMirror/PreProcessors/WixsitePreprocess.py
@@ -17,13 +17,13 @@ class JsRendererPreprocessor(WebMirror.PreProcessors.PreProcessorBase.ContentPre
 
 	def preprocessContent(self, url, mimetype, contentstr):
 		soup = common.util.WebRequest.as_soup(contentstr)
-		text = soup.get_text(strip=True)
+		text = soup.body.get_text(strip=True).strip()
 
-		if len(text) < 100:
+		if len(text) < 100 or True:
 			self.log.info("Page has little or no body. Trying to refetch and render using chromium.")
 			contentstr, dummy_fileN, dummy_mType = self.wg.chromiumGetRenderedItem(url)
 		else:
-			self.log.info("Page has body, no re-fetch & render needed.")
+			self.log.info("Page has %s char body, no re-fetch & render needed.", len(text))
 
 		return contentstr
 
