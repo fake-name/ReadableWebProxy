2b71559fa3ee23bd112205f87e67118f093675f8
Fake-Name <something@fake-url.com>
2018-05-04 01:57:52 -0700

    Urrrgh. https://bugs.launchpad.net/beautifulsoup/+bug/972466

--- WebMirror/PreProcessors/TgStoryTimePreprocess.py
+++ WebMirror/PreProcessors/TgStoryTimePreprocess.py
@@ -25,12 +25,14 @@ class TgStoryTimePreprocessor(WebMirror.PreProcessors.PreProcessorBase.ContentPr
 		tgt = urllib.parse.urljoin(url, newloc)
 		new = self.wg.getpage(tgt)
 		new = self.wg.getpage(url)
-		
+
 		assert 'This story has explicit content.' not in new
 		return new
 
 
 	def preprocessContent(self, url, mimetype, contentstr):
+
+
 		if not isinstance(contentstr, str):
 			return contentstr
 		self.log.info("Preprocessing content from URL: '%s'", url)
@@ -39,9 +41,12 @@ class TgStoryTimePreprocessor(WebMirror.PreProcessors.PreProcessorBase.ContentPr
 			self.log.info("Adult clickwrap page. Stepping through")
 			contentstr = self.acceptAdult(contentstr, url)
 			self.log.info("Retreived clickwrapped content successfully")
+
+		contentstr = contentstr.replace("charset=ISO-8859-1", 'charset=UTF-8')
 		return contentstr
 
 	@staticmethod
 	def wantsUrl(url):
+		print("WantsURL: ", url)
 		netloc = urllib.parse.urlsplit(url).netloc
 		return netloc.lower().endswith("tgstorytime.com")
